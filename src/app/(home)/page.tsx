df2f1145929d82b3c9464ba1e7ec8e75e186fdaa
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-01-10 21:30:34 -0500

    Add environment variables for Shopify GraphQL endpoint and storefront token Update metadata for home page Add generateMetadata function for product page Redirect to /store if id is not provided in ProductPage SanitizeHTML

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Future world",
+  description: "future world",
+  keywords: ["future", "world", "future world"],
+};
 export default function Home() {
   return (
     <main>
--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
-import styles from './ProductView.module.sass'
+import { SanitizeHTML } from "app/components/shared/SanitizeHTML";
+import styles from "./ProductView.module.sass";
 interface ProductViewProps {
-  product: ProductType
+  product: ProductType;
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
-
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
@@ -22,14 +22,12 @@ export const ProductView = ({ product }: ProductViewProps) => {
       <section className={styles.ProductView__info}>
         <h1 className={styles.ProductView__info__title}>{product.title}</h1>
         <p className={styles.ProductView__info__category}>{product.tags}</p>
-        <p className={styles.ProductView__info__description}>
-          {product.description}
-        </p>
+        <SanitizeHTML tag="p">{product.description}</SanitizeHTML>
         <span className={styles.ProductView__info__price}>
           $ {product.price}
         </span>
         <ProductViewItemsOrder maxQuantity={product.quantity} />
       </section>
     </main>
-  )
+  );
 };
